4d35207508827b3bde3ae1c2da9008f56fac3cdb
Networksentinel <network.sentinel@protonmail.com>
2025-05-02 09:45:35 +0200

    docs: setup vite config file

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+console.log("vite.config.js is loaded!");
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Networksentinel-Frontend-Mentor-Challenge---Blog-preview-card/',
+});
